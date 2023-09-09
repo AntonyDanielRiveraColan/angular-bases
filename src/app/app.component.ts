12ff1f2152916e70efdf96953e1582dcee1a6811
Antony Daniel Rivera Colan <50754648+AntonyDanielRiveraColan@users.noreply.github.com>
2023-09-09 15:42:15 -0500

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: String = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy():void{
+    this.counter +=1;
+  }
+
+  minBy():void{
+    this.counter -=1;
+  }
+
+  reset():void{
+    this.counter =10;
+  }
 }
